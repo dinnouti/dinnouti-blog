8515da3b002fd53c4b617141294a39f77db5bfda
Alex Dinnouti <dinnoua@gmail.com>
2021-02-21 16:08:26 -0500

    deploying S3 commit at 2021-02-21 16:08:26

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,28 @@
+// module.exports = {
+//     plugins: [require("tailwindcss"), require("autoprefixer")],
+//   };
+
+const themeDir = __dirname + '/../../';
+
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    // see https://gohugo.io/hugo-pipes/postprocess/#css-purging-with-postcss
+    content: ['./hugo_stats.json'],
+    defaultExtractor: (content) => {
+        let els = JSON.parse(content).htmlElements;
+        return els.tags.concat(els.classes, els.ids);
+    }
+})
+
 module.exports = {
-    plugins: [require("tailwindcss"), require("autoprefixer")],
-  };
+    plugins: [
+        require('postcss-import')({
+            path: [themeDir]
+            }),
+        require('tailwindcss')(themeDir + 'assets/css/tailwind.config.js'),
+        require('autoprefixer')({
+            path: [themeDir],
+            grid: true
+        }),
+        ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : [])
+    ]
+}
